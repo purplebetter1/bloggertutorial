dc8a653d23424ecb50673c6dfaaec7bac248389c
EC2 Default User <ec2-user@ip-172-31-26-226.us-east-2.compute.internal>
2019-11-06 17:53:17 +0000

    Add Article model and controller

--- db/migrate/20191106171021_create_articles.rb
+++ db/migrate/20191106171021_create_articles.rb
@@ -1,8 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[5.1]
   def change
-    create_table :articles do |t|
+  create_table :articles do |t|
+    t.string :title
+    t.text :body
 
-      t.timestamps
-    end
+    t.timestamps
+   end
   end
 end
